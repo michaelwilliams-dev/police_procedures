329d5ba0905c5090418e9055f0c919be8cb61bdb
MichaelW <michael@justresults.co>
2025-04-14 15:03:54 +0100

    Fix: run gunicorn via python3 module

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Render always starts this from the root folder
 
 echo "Starting Gunicorn..."
-exec gunicorn api:app --bind 0.0.0.0:10000
+exec python3 -m gunicorn api:app --bind 0.0.0.0:10000
